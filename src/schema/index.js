c3f75a279862877f5850079fc6a4dfd823d6a65a
Cameron Hunter <chunter@twitter.com>
2016-02-16 16:59:19 -0800

    Rename types to schema and move schema.js into the package as index.js

---
+++ src/schema/index.js
@@ -0,0 +1,8 @@
+export * as audio from './audio';
+export * as pause from './pause';
+export * as p from './p';
+export * as phoneme from './phoneme';
+export * as s from './s';
+export * as sayas from './sayAs';
+export * as speak from './speak';
+export * as w from './w';
