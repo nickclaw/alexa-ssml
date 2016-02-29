b198d73c6c430534bdb48d1a7b3a2092a0ebd6b4
Cameron Hunter <chunter@twitter.com>
2016-02-28 22:38:18 -0800

    Tidy up validation

--- src/validateProps.js
+++ src/validateProps.js
@@ -1,5 +1,4 @@
 import tv4 from 'tv4';
-import omit from 'lodash/omit';
 
 /**
  * Validates properties against a schema
@@ -8,22 +7,21 @@ import omit from 'lodash/omit';
  * @return {Object} validated props
  */
 export default function validateProps(props, schema) {
-    const itemProps = omit(props, 'children'); // TODO add child count validation too?
+    const { children, ...itemProps } = props || {}; // TODO add child count validation too?
     const { error } = tv4.validateResult(itemProps, schema, false, true);
 
     if (error) {
-        if (error.dataPath) {
-            const propName = error.dataPath.slice(1);
-            throw new Error(`Unknown property: ${propName}`);
+        const { dataPath, srcPath, message } = error;
+
+        if (dataPath) {
+            throw new Error(`Unknown property: ${dataPath.slice(1)}`);
         }
 
-        if (error.srcPath) {
-            const propName = error.srcPath.slice(1);
-            const reason = error.message;
-            throw new Error(`Invalid value for property ${propName}: ${reason}`);
+        if (srcPath) {
+            throw new Error(`Invalid value for property ${srcPath.slice(1)}: ${message}`);
         }
 
-        throw new Error(error.message);
+        throw new Error(message);
     }
 
     return itemProps;
